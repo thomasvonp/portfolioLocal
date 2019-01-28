06efb7a40452c1321c49e67bee420f840a8668b4
Zindae <thomasvp@kth.se>
2019-01-28 15:34:45 +0100

    rev

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const path = require('path');
 
 const app = express();
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/portfolioLocal'));
 
